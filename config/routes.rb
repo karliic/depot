c4534827dac5e196dcaaffa497039d28b85d2c26
Karlis <karlis@karlis-VirtualBox.(none)>
2013-06-05 14:52:39 +0300

    finish task a

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Depot::Application.routes.draw do
+  resources :products
+
+  root :to => 'products#index'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
