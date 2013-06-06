8c74fa707c4cc9c6d6bb05fe363009a4949d8521
Karlis <karlis@karlis-VirtualBox.(none)>
2013-06-06 12:39:01 +0300

    finish task d

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = access_counter
+  end
+
+  def access_counter
+    session[:counter] ||= 0
+    session[:counter] += 1
   end
 end
